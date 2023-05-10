5cf3a0476f2539a00b06dce01d6653b03f778cf6
EVA <ccano@grupohdi.com>
2023-05-10 14:52:23 -0400

    actualizada prueba unitaria test_get_propertie

--- src/com/SendMessgCanTest.py
+++ src/com/SendMessgCanTest.py
@@ -4,15 +4,17 @@ from lib import rmdx_funtions  as rmdx
 import configs.CaptureConfigs as cf
 
 class SendMess(unittest.TestCase):
-    def send_mess_can(self):
+
+    def test_get_propertie(self):
         header = 'codeTypeAccionHex'
         param = 'send.torque'
         data = cf.getValueConfig(header,param)
         data_goal = [0xA1, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        self.assertAlmostEqual(data,data_goal)
-
+        print(data)
+        self.assertEqual(data,data_goal)
+        
 
 if __name__ == "__main__":
-    unittest.main
+    unittest.main()
 #--------------------------------------------
 
